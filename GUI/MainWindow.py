4278d38d68e2983d76a69f61041a82dc7707642f
Username <vova-shaposh@mail.ru>
2021-09-13 02:48:41 +0300

    Adding angles, and multicameras supporting

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -3,18 +3,21 @@ import sys
 
 import cv2
 import mediapipe as mp
+import numpy as np
+import copy
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import *
 
 
 class Thread(QThread):
-    changePixmap = pyqtSignal(QImage)
-    changeText = pyqtSignal(str)
+    changePixmap = pyqtSignal(tuple)
+    changeText = pyqtSignal(list)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, cam_num, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.cam = cam_num
         self.mpPose = mp.solutions.pose
         self.pose = self.mpPose.Pose()
         self.mpDraw = mp.solutions.drawing_utils
@@ -42,77 +45,139 @@ class Thread(QThread):
 
         return landmarks
 
+    def get_angle(self, landmark, joint_list):
+        angle_list = []
+        for joint in joint_list:
+            a = np.array([landmark[joint[0]].x,landmark[joint[0]].y]) # First coordinate
+            b = np.array([landmark[joint[1]].x,landmark[joint[1]].y]) # Second coordinate
+            c = np.array([landmark[joint[2]].x,landmark[joint[2]].y]) # Third coordinate
+
+            radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+            angle = np.abs(radians*180.0/np.pi)
+            angle = angle - 180 if angle > 180.0 else angle
+
+            angle_list.append(angle)
+
+
+        return angle_list
+
+    def video_preapring(self, frame, width=640, height=480):
+        """
+        This function processes video and converting it to QtVideoFormat
+
+        Ids from doc: https://google.github.io/mediapipe/solutions/pose.html
+
+        :return: found landmarks
+        """
+        rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgbImage.shape
+        bytesPerLine = ch * w
+        convertToQtFormat = QImage(
+            rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888
+        )
+        return convertToQtFormat.scaled(width, height, Qt.KeepAspectRatio)
+
     def run(self):
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(self.cam)
 
         while True:
             ret, frame = cap.read()
 
             if ret:
+                clearQtImage = self.video_preapring(copy.deepcopy(frame))
                 landmarks = self.get_new_frame_from_neuron(frame)
-                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                markedQtImage = self.video_preapring(frame)
+                imageTuple = (clearQtImage, markedQtImage)
+                self.changePixmap.emit(imageTuple)
+                if landmarks:
+                    angList = self.get_angle(landmarks, ([16,14,12], [14,12,24], [15,13,11], [13, 11, 23]))
+                    self.changeText.emit(angList)
 
-                h, w, ch = rgbImage.shape
-                bytesPerLine = ch * w
-                convertToQtFormat = QImage(
-                    rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888
-                )
-                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
-                self.changePixmap.emit(p)
-                bright = (
-                        int(frame[320, 240, 0]) +
-                        int(frame[320, 240, 1]) +
-                        int(frame[320, 240, 2])
-                )
-                print(frame[320, 240, 0], frame[320, 240, 1],
-                      frame[320, 240, 2], bright)
+                # bright = (
+                #         int(frame[320, 240, 0]) +
+                #         int(frame[320, 240, 1]) +
+                #         int(frame[320, 240, 2])
+                # )
+                # print(frame[320, 240, 0], frame[320, 240, 1],
+                #       frame[320, 240, 2], bright)
+                #
+                # if bright > 400:
+                #     self.changeText.emit('Too bright')
+                # elif bright < 120:
+                #     self.changeText.emit('Too dark')
+                # else:
+                #     self.changeText.emit('Everything is Okay')
 
-                if bright > 400:
-                    self.changeText.emit('Too bright')
-                elif bright < 120:
-                    self.changeText.emit('Too dark')
-                else:
-                    self.changeText.emit('Everything is Okay')
 
 
 class App(QWidget):
-    def __init__(self, thread_parent=None):
+    def __init__(self, cam_id=0):
         super().__init__()
+        self.cam_id = cam_id
         self.initUI()
 
-    @pyqtSlot(QImage)
-    def setImage(self, image):
-        self.label.setPixmap(QPixmap.fromImage(image))
+    @pyqtSlot(tuple)
+    def setImage(self, imageTuple):
+        self.labelClear.setPixmap(QPixmap.fromImage(imageTuple[0]))
+        self.labelMarked.setPixmap(QPixmap.fromImage(imageTuple[1]))
 
-    @pyqtSlot(str)
-    def setText(self, text):
-        self.picture_indicator.setText(text)
+    @pyqtSlot(list)
+    def setText(self, elementList):
+        elements = (self.rightHandElbow, self.rightHandShoulder, self.leftHandElbow, self.leftHandShoulder)
+        for pos, el in enumerate(elements):
+            el.setText(f'{elementList[pos]:.2f}')
 
-    def initUI(self):
+    def create_text_instance(self, labelName: QLabel, labelComment: str):
+        frame = QFrame(self)
         hbox = QHBoxLayout()
 
-        self.label = QLabel()
+        comment = QLabel(self)
+        comment.setText(labelComment)
+        hbox.addWidget(comment)
+
+        hbox.addWidget(labelName)
+
+        frame.setLayout(hbox)
+        return frame
+
+
+
+    def initUI(self):
+
+
+        hbox = QHBoxLayout()
+
+        self.labelClear = QLabel()
+        self.labelMarked = QLabel()
         self.setWindowTitle('Camera')
         # create a label
-        hbox.addWidget(self.label)
+        hbox.addWidget(self.labelClear)
+        hbox.addWidget(self.labelMarked)
 
         vbox = QVBoxLayout()
         frame = QFrame(self)
 
-        self.resolution = QLabel(self)
-        self.resolution.setText('640x480')
-        vbox.addWidget(self.resolution)
 
-        self.picture_indicator = QLabel(self)
-        self.picture_indicator.setText('Everything is okay')
-        vbox.addWidget(self.picture_indicator)
+        self.rightHandElbow = QLabel(self)
+        vbox.addWidget(self.create_text_instance(self.rightHandElbow, 'Right hand elbow:'))
+
+        self.rightHandShoulder = QLabel(self)
+        vbox.addWidget(self.create_text_instance(self.rightHandShoulder, 'Right hand shoulder:'))
+
+
+        self.leftHandElbow = QLabel(self)
+        vbox.addWidget(self.create_text_instance(self.leftHandElbow, 'Left hand elbow:'))
+
+        self.leftHandShoulder = QLabel(self)
+        vbox.addWidget(self.create_text_instance(self.leftHandShoulder, 'Left hand shoulder:'))
+
 
         frame.setLayout(vbox)
         hbox.addWidget(frame)
         self.setLayout(hbox)
 
         self.setGeometry(300, 100, 200, 200)
-        th = Thread(parent=self)
+        th = Thread(cam_num=self.cam_id, parent=self)
         th.changePixmap.connect(self.setImage)
         th.changeText.connect(self.setText)
         th.start()
@@ -150,7 +215,6 @@ class MainWindow(QMainWindow):
         exit.clicked.connect(lambda: QCoreApplication.instance().quit())
         vbox.addWidget(exit)
         buttons_widget.setLayout(vbox)
-        buttons_widget.setLayout(vbox)
         hbox.addWidget(buttons_widget)
 
         main_wigh.setLayout(hbox)
@@ -159,11 +223,35 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Main Window')
         self.show()
 
+    def return_camera_indexes(self, start=10):
+        # checks the first 10 indexes.
+        index = 0
+        arr = []
+        i = start
+        while i > 0:
+            cap = cv2.VideoCapture(index)
+            if cap.read()[0]:
+                arr.append(index)
+                cap.release()
+            index += 1
+            i -= 1
+        return arr
+
+
     def camera_launch(self):
         # w = App(thread_parent=self)
-        if self.w is None:
-            self.w = App()
-            self.w.show()
+        cameras = self.return_camera_indexes(4)
+        # if self.w is None:
+        # for el in cameras:
+        self.w = App(cameras[0])
+        self.w.show()
+
+        self.c = App(cameras[1])
+        self.c.show()
+
+        # lst = []
+        # lst.append(App())
+        # lst[0].show()
 
         # vbox.addWidget(self.picture_in_box(self.resource_path("pictures/logo_mini.jpg"), 150))
 
